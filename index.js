440ae06a55d1b8ece8dd301c075595b45441ed71
jomapormentilla <jomapormentilla@gmail.com>
2021-01-28 20:35:15 -0500

    commit tree 6e239eb564b7d4a1da038e0c8d56de1a9874a506

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+const url = "http://localhost:3000/users"
+
+function submitData( name, email ){
+    const formData = {
+        name: name,
+        email: email
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch( url, configObj )
+        .then( response=>response.json() )
+        .then( json => {
+            document.body.innerHTML += json["id"]
+        })
+        .catch( error => {
+            document.body.innerHTML += error.message
+        })
+}
+
+function getData(){
+    fetch(url)
+        .then( response => response.json() )
+        .then( json => {
+            for( const keys in json ){
+                document.body.innerHTML += json[keys]["id"]
+            }
+        })
+        .catch( error => {
+            document.body.innerHTML =+ error.message
+        })
+}
+
+getData()
